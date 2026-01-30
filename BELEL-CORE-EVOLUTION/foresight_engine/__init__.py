f4a778cdd8d4b77baacaa3816cac7a3c286d2f92
Office of Pearce Robinson <215385919+TTOPM@users.noreply.github.com>
2026-01-30 07:54:00 +0000

    Update __init__.py

--- BELEL-CORE-EVOLUTION/foresight_engine/__init__.py
+++ BELEL-CORE-EVOLUTION/foresight_engine/__init__.py
@@ -1,1 +1,5 @@
-"""Package."""
+from .world_model import WorldModel
+from .digital_twin import DigitalTwin
+from .orchestrator import ForesightOrchestrator
+
+__all__ = ["WorldModel", "DigitalTwin", "ForesightOrchestrator"]
