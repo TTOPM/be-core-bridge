28556c75f47ac4669d4c2b388498ef346914e244
Office of Pearce Robinson <215385919+TTOPM@users.noreply.github.com>
2026-01-30 08:02:00 +0000

    Update __init__.py

--- BELEL-CORE-EVOLUTION/governance_filters/__init__.py
+++ BELEL-CORE-EVOLUTION/governance_filters/__init__.py
@@ -1,1 +1,4 @@
-"""Package."""
+from .filters import evaluate_upgrade_request
+from .concordium_checks import concordium_invariants_ok
+
+__all__ = ["evaluate_upgrade_request", "concordium_invariants_ok"]
