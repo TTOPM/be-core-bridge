489831b4f1dfd8f2c789933c58d83c7fd54f48ec
Office of Pearce Robinson <215385919+TTOPM@users.noreply.github.com>
2026-02-05 01:40:00 +0000

    Create page.tsx

---
+++ BELEL-SING/belel-sing-gen/apps/belel-studio/app/library/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LibraryPage } from "@/components/studio/LibraryPage";
+
+export default function Library() {
+  return (
+    <div className="p-6">
+      <LibraryPage />
+    </div>
+  );
+}
