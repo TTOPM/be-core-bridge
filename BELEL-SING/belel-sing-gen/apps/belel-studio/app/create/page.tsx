c5bdc69404c9795623d778db2d83898ebacd7805
Office of Pearce Robinson <215385919+TTOPM@users.noreply.github.com>
2026-02-05 01:38:23 +0000

    Create page.tsx

---
+++ BELEL-SING/belel-sing-gen/apps/belel-studio/app/create/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CreatePanel } from "@/components/create/CreatePanel";
+
+export default function CreatePage() {
+  return (
+    <div className="p-6">
+      <CreatePanel />
+    </div>
+  );
+}
