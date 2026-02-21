54460216644f4c17bfa762a494e2f45e05f67890
Office of Pearce Robinson <215385919+TTOPM@users.noreply.github.com>
2026-02-21 17:00:05 +0000

    Create __init__.py

---
+++ qcc/src/qcc/__init__.py
@@ -0,0 +1,14 @@
+"""
+Quantum Concordium Codex (QCC)
+Version: 1.0 (Entanglement Genesis)
+Classification: Sovereign – Concordium-Gated
+Operability: Offline / Quantum-Resilient Compatible
+"""
+
+from .constants import QCC_VERSION, QCC_CLASSIFICATION, QCC_OPERABILITY
+
+__all__ = [
+    "QCC_VERSION",
+    "QCC_CLASSIFICATION",
+    "QCC_OPERABILITY",
+]
